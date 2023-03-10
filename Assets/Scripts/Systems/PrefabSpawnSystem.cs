ed68a0b45c09476520c256b13985e718e9eaee24
vyacheslav.ryndin <waterneverstops@gmail.com>
2023-03-10 14:31:07 +0300

    Added prefabSetup with cameraPrefab and PrefabSpawnSystem

---
+++ Assets/Scripts/Systems/PrefabSpawnSystem.cs
@@ -0,0 +1,20 @@
+﻿using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+using TownBuilder.MonoComponents;
+
+namespace TownBuilder.Systems
+{
+    public class PrefabSpawnSystem : IEcsInitSystem
+    {
+        private readonly EcsCustomInject<PrefabFactory> _prefabFactoryInjection = default;
+
+        private PrefabFactory _prefabFactory;
+        
+        public void Init(IEcsSystems systems)
+        {
+            _prefabFactory = _prefabFactoryInjection.Value;
+            
+            _prefabFactory.SetWorld(systems.GetWorld());
+        }
+    }
+}
