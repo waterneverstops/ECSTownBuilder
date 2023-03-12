45e1471158222debc9a770981a12e13a601d97bf
vyacheslav.ryndin <waterneverstops@gmail.com>
2023-03-12 17:49:44 +0300

    Code CleanUp and removed debug pathbuilder code

--- Assets/Scripts/Systems/Building/PathBuilderSystem.cs
+++ Assets/Scripts/Systems/Building/PathBuilderSystem.cs
@@ -4,14 +4,12 @@ using TownBuilder.Components;
 using TownBuilder.Components.Building;
 using TownBuilder.Components.Input;
 using TownBuilder.Context;
-using TownBuilder.SO;
 
 namespace TownBuilder.Systems.Building
 {
     public class PathBuilderSystem : IEcsInitSystem, IEcsRunSystem
     {
         private readonly EcsCustomInject<LevelContext> _levelContextInjection = default;
-        private readonly EcsCustomInject<PrefabSetup> _prefabSetupInjection = default;
 
         private MapGrid _mapGrid;
         private MapGridPathfinder _gridPathfinder;
@@ -20,16 +18,6 @@ namespace TownBuilder.Systems.Building
         {
             _mapGrid = _levelContextInjection.Value.MapGrid;
             _gridPathfinder = new MapGridPathfinder();
-
-            var world = systems.GetWorld();
-            var builderEntity = world.NewEntity();
-            var builderPool = world.GetPool<Builder>();
-            var pathPool = world.GetPool<BuildPath>();
-
-            ref var builderComponent = ref builderPool.Add(builderEntity);
-            builderComponent.Prefab = _prefabSetupInjection.Value.RoadPrefabSetup.BaseRoadPrefab;
-
-            pathPool.Add(builderEntity);
         }
 
         public void Run(IEcsSystems systems)
@@ -38,30 +26,28 @@ namespace TownBuilder.Systems.Building
 
             var builderFilter = world.Filter<Builder>().Inc<BuildPath>().End();
             var mouseInputFilter = world.Filter<MousePressed>().Inc<MouseReleased>().End();
-            
+
             foreach (var builderEntity in builderFilter)
+            foreach (var mouseInputEntity in mouseInputFilter)
             {
-                foreach (var mouseInputEntity in mouseInputFilter)
+                var pressedPool = world.GetPool<MousePressed>();
+                var releasedPool = world.GetPool<MouseReleased>();
+                var pressedPosition = pressedPool.Get(mouseInputEntity).Position;
+                var releasedPosition = releasedPool.Get(mouseInputEntity).Position;
+
+                var path = _gridPathfinder.GetAStarSearchPath(_mapGrid, pressedPosition, releasedPosition);
+                if (path.Count == 0) continue;
+
+                var builderPool = world.GetPool<Builder>();
+                var prefab = builderPool.Get(builderEntity).Prefab;
+
+                foreach (var buildPosition in path)
                 {
-                    var pressedPool = world.GetPool<MousePressed>();
-                    var releasedPool = world.GetPool<MouseReleased>();
-                    var pressedPosition = pressedPool.Get(mouseInputEntity).Position;
-                    var releasedPosition = releasedPool.Get(mouseInputEntity).Position;
-
-                    var path = _gridPathfinder.GetAStarSearchPath(_mapGrid, pressedPosition, releasedPosition);
-                    if (path.Count == 0) continue;
-                    
-                    var builderPool = world.GetPool<Builder>();
-                    var prefab = builderPool.Get(builderEntity).Prefab;
-
-                    foreach (var buildPosition in path)
-                    {
-                        var spawnEntity = world.NewEntity();
-                        var spawnPool = world.GetPool<SpawnPrefabGrid>();
-                        ref var spawnComponent = ref spawnPool.Add(spawnEntity);
-                        spawnComponent.Position = buildPosition;
-                        spawnComponent.Prefab = prefab;    
-                    }
+                    var spawnEntity = world.NewEntity();
+                    var spawnPool = world.GetPool<SpawnPrefabGrid>();
+                    ref var spawnComponent = ref spawnPool.Add(spawnEntity);
+                    spawnComponent.Position = buildPosition;
+                    spawnComponent.Prefab = prefab;
                 }
             }
         }
