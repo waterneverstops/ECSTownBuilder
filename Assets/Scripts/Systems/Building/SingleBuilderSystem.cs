9ba648430b713633db7af97db059349023769526
vyacheslav.ryndin <waterneverstops@gmail.com>
2023-03-12 17:26:58 +0300

    Removed debug single builder system code

--- Assets/Scripts/Systems/Building/SingleBuilderSystem.cs
+++ Assets/Scripts/Systems/Building/SingleBuilderSystem.cs
@@ -4,30 +4,18 @@ using TownBuilder.Components;
 using TownBuilder.Components.Building;
 using TownBuilder.Components.Input;
 using TownBuilder.Context;
-using TownBuilder.SO;
 
 namespace TownBuilder.Systems.Building
 {
     public class SingleBuilderSystem : IEcsInitSystem, IEcsRunSystem
     {
         private readonly EcsCustomInject<LevelContext> _levelContextInjection = default;
-        private readonly EcsCustomInject<PrefabSetup> _prefabSetupInjection = default;
 
         private MapGrid _mapGrid;
         
         public void Init(IEcsSystems systems)
         {
             _mapGrid = _levelContextInjection.Value.MapGrid;
-
-            var world = systems.GetWorld();
-            var builderEntity = world.NewEntity();
-            var builderPool = world.GetPool<Builder>();
-            var singlePool = world.GetPool<BuildSingle>();
-
-            ref var builderComponent = ref builderPool.Add(builderEntity);
-            builderComponent.Prefab = _prefabSetupInjection.Value.RoadPrefabSetup.BaseRoadPrefab;
-
-            singlePool.Add(builderEntity);
         }
 
         public void Run(IEcsSystems systems)
