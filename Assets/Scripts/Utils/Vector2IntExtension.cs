f3315f861644fe899807716a428f79d4c43495a9
vyacheslav.ryndin <waterneverstops@gmail.com>
2023-03-12 17:20:34 +0300

    Vector2Int extension class

---
+++ Assets/Scripts/Utils/Vector2IntExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace TownBuilder.Utils
+{
+    namespace Extensions
+    {
+        public static class Vector2IntExtension
+        {
+            public static float ManhattanDistance(this Vector2Int start, Vector2Int end)
+            {
+                return Math.Abs(end.x - start.x) + Math.Abs(end.y - start.y);
+            }
+        }
+    }
+}
