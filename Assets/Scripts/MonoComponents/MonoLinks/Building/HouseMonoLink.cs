5ff0200862cad599e1f345dd23141163479e65c0
vyacheslav.ryndin <waterneverstops@gmail.com>
2023-03-14 15:40:28 +0300

    components and monolinks for houses. markets. hunterhuts

---
+++ Assets/Scripts/MonoComponents/MonoLinks/Building/HouseMonoLink.cs
@@ -0,0 +1,7 @@
+﻿using TownBuilder.Components.Grid;
+using TownBuilder.MonoComponents.MonoLinks.Base;
+
+namespace TownBuilder.MonoComponents.MonoLinks.Building
+{
+    public class HouseMonoLink : MonoLink<House> { }
+}
