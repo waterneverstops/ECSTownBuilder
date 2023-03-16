c40d0f744231070b14d5a61dea16c3cad84d384f
vyacheslav.ryndin <waterneverstops@gmail.com>
2023-03-16 17:05:49 +0300

    StructureStorage component

---
+++ Assets/Scripts/Components/Structures/StructureStorage.cs
@@ -0,0 +1,7 @@
+﻿namespace TownBuilder.Components.Structures
+{
+    public struct StructureStorage
+    {
+        public int Food;
+    }
+}
---
+++ Assets/Scripts/MonoComponents/MonoLinks/Structures/StructureStorageMonoLink.cs
@@ -0,0 +1,7 @@
+﻿using TownBuilder.Components.Structures;
+using TownBuilder.MonoComponents.MonoLinks.Base;
+
+namespace TownBuilder.MonoComponents.MonoLinks.Structures
+{
+    public class StructureStorageMonoLink : MonoLink<StructureStorage> { }
+}
