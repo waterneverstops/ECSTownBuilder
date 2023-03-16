9ab99fbc105ce6b517644c875d98772f421605d3
vyacheslav.ryndin <waterneverstops@gmail.com>
2023-03-16 20:40:23 +0300

    maxStorage component

---
+++ Assets/Scripts/Components/Structures/StructureMaxStorage.cs
@@ -0,0 +1,7 @@
+﻿namespace TownBuilder.Components.Structures
+{
+    public struct StructureMaxStorage
+    {
+        public int MaxFood;
+    }
+}
---
+++ Assets/Scripts/MonoComponents/MonoLinks/Structures/StructureMaxStorageMonoLink.cs
@@ -0,0 +1,25 @@
+﻿using Leopotam.EcsLite;
+using TownBuilder.Components.Structures;
+using TownBuilder.MonoComponents.MonoLinks.Base;
+using UnityEngine;
+
+namespace TownBuilder.MonoComponents.MonoLinks.Structures
+{
+    public class StructureMaxStorageMonoLink : MonoLink<StructureMaxStorage>
+    {
+        [SerializeField] private int _maxFood;
+
+        public override void Make(EcsPackedEntityWithWorld packedEntityWithWorld)
+        {
+            if (packedEntityWithWorld.Unpack(out var world, out var entity))
+            {
+                var pool = world.GetPool<StructureMaxStorage>();
+
+                if (pool.Has(entity)) return;
+
+                ref var component = ref pool.Add(entity);
+                component.MaxFood = _maxFood;
+            }
+        }
+    }
+}
