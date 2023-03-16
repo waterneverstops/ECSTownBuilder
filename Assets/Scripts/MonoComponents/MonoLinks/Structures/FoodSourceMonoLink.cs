a0807d6b435aee7847ed2f830ab7f19fa9670a4a
vyacheslav.ryndin <waterneverstops@gmail.com>
2023-03-16 19:03:38 +0300

    Added indestructible on food source

--- Assets/Scripts/MonoComponents/MonoLinks/Structures/FoodSourceMonoLink.cs
+++ Assets/Scripts/MonoComponents/MonoLinks/Structures/FoodSourceMonoLink.cs
@@ -1,8 +1,9 @@
-﻿using TownBuilder.Components.Grid;
-using TownBuilder.Components.Structures;
+﻿using TownBuilder.Components.Structures;
 using TownBuilder.MonoComponents.MonoLinks.Base;
 
 namespace TownBuilder.MonoComponents.MonoLinks.Structures
 {
-    public class FoodSourceMonoLink : MonoLink<FoodSource> { }
+    public class FoodSourceMonoLink : MonoLink<FoodSource>
+    {
+    }
 }
---
+++ Assets/Scripts/MonoComponents/MonoLinks/Structures/IndestructibleMonoLink.cs
@@ -0,0 +1,7 @@
+﻿using TownBuilder.Components;
+using TownBuilder.MonoComponents.MonoLinks.Base;
+
+namespace TownBuilder.MonoComponents.MonoLinks.Structures
+{
+    public class IndestructibleMonoLink : MonoLink<Indestructible> { }
+}
